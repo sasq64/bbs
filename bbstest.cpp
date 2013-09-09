13351def6c2a7550e7070ae947d6aa4255fe9bd0
Jonas Minnberg <sasq64@gmail.com>
2013-09-09 19:50:44 +0200

    Console changes

--- bbstest.cpp
+++ bbstest.cpp
@@ -44,7 +44,6 @@ int main(int argc, char **argv) {
 	telnet.setOnConnect([&](TelnetServer::Session &session) {
 		session.echo(false);
 		auto termType = session.getTermType();		
-		LOGD("New connection, TERMTYPE '%s'", termType);
 
 		unique_ptr<Console> console;
 		if(termType.length() > 0) {
@@ -52,9 +51,10 @@ int main(int argc, char **argv) {
 		} else {
 			console = unique_ptr<Console>(new PetsciiConsole { session });
 		}
-		//console->flush();
+		console->flush();
 		auto h = console->getHeight();
 		auto w = console->getWidth();
+		LOGD("New connection, TERMTYPE '%s' SIZE %dx%d", termType, w, h);
 
 		console->write("\nNAME:");
 		auto userName = console->getLine();
@@ -63,7 +63,7 @@ int main(int argc, char **argv) {
 		int lastLine = 0;
 		int ypos = 0;
 		console->clear();
-		console->put(0, h-2, string(w,'-'));
+		console->put(0, h-2, string(w,'-'), Console::CURRENT_COLOR, Console::BLUE);
 		console->moveCursor(0, h-1);
 		
 		auto line = console->getLineAsync();
@@ -77,7 +77,7 @@ int main(int argc, char **argv) {
 				{ lock_guard<mutex> guard(chatLock);
 					chatLines.push_back(userName + ": " + line.get());
 				}
-				console->put(0, h-1, string(w,' '));
+				console->put(0, h-1, string(w,' '), Console::CURRENT_COLOR, Console::BLACK);
 				console->moveCursor(0,h-1);
 				line = console->getLineAsync();
 			}
@@ -88,8 +88,8 @@ int main(int argc, char **argv) {
 					ypos++;
 					if(ypos > h-2) {
 						console->scrollScreen(1);
-						console->put(0, h-3, string(w,' '));
-						console->put(0, h-2, string(w,'-'));
+						console->put(0, h-3, string(w,' '), Console::CURRENT_COLOR, Console::BLACK);
+						console->put(0, h-2, string(w,'-'), Console::CURRENT_COLOR, Console::BLUE);
 						ypos--;
 					}
 					console->put(0, ypos-1, msg);
